d67f4be3ae5faf07d3908d85e7127617e04db3c9
GLGDLY <tzlgdly@gmail.com>
2024-10-25 19:50:04 +0800

    update code structure, enable wtd back

--- main/src/spi_gpio_helper.c
+++ main/src/spi_gpio_helper.c
@@ -98,7 +98,7 @@ gptimer_handle_t timer = NULL;
 bool timer_isr_handler(struct gptimer_t* timer, const gptimer_alarm_event_data_t* event, void* arg) {
 	static bool gpio_state = false;
 	static uint8_t low_cnt = 1;
-	if (gpio_state == 0 && low_cnt >= 30) {
+	if (gpio_state == 0 && low_cnt >= SPI_SYNC_LOW_TIME_ALARM_COUNT) {
 		gpio_state = !gpio_state;
 	} else if (gpio_state == 1) {
 		gpio_state = !gpio_state;
@@ -106,12 +106,13 @@ bool timer_isr_handler(struct gptimer_t* timer, const gptimer_alarm_event_data_t
 	}
 	gpio_set_level(SPI_SYNC_PIN, !gpio_state);
 
-	// if (gpio_state == 0) {
-	// 	if (low_cnt++ == 0) { // falling edge
-	// 		spi_sync_falling_edge_handler(arg);
-	// 		return true;
-	// 	}
-	// }
+	if (gpio_state == 0) {
+		// 	if (low_cnt++ == 0) { // falling edge
+		// 		spi_sync_falling_edge_handler(arg);
+		// 		return true;
+		// 	}
+		low_cnt++;
+	}
 	return false;
 }
 
@@ -148,7 +149,7 @@ void spi_sync_init(void) {
 
 	gptimer_alarm_config_t alarm_config = {
 		.reload_count = 0,
-		.alarm_count = 100,
+		.alarm_count = SPI_SYNC_ALARM_US,
 		.flags.auto_reload_on_alarm = true,
 	};
 	ret = gptimer_set_alarm_action(timer, &alarm_config);
